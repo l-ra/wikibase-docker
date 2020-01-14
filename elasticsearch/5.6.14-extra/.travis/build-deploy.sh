2e5dd27d60ee14632771aedc8370eeccaf2f1cb3
lra <lra@r73.info>
2020-01-14 23:13:29 +0100

    docker build changes

--- elasticsearch/5.6.14-extra/.travis/build-deploy.sh
+++ elasticsearch/5.6.14-extra/.travis/build-deploy.sh
@@ -2,9 +2,9 @@
 #Oneline for full directory name see: https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -e
-docker build "$DIR/../" -t wikibase/elasticsearch:5.6.14-extra -t wikibase/elasticsearch:latest
+docker build "$DIR/../" -t ${DOCKER_REPO}/elasticsearch:5.6.14-extra -t ${DOCKER_REPO}/elasticsearch:latest
 
 if [ "$SHOULD_DOCKER_PUSH" = true ]; then
-    docker push wikibase/elasticsearch:5.6.14-extra
-    docker push wikibase/elasticsearch:latest
+    docker push ${DOCKER_REPO}/elasticsearch:5.6.14-extra
+    docker push ${DOCKER_REPO}/elasticsearch:latest
 fi
--- wikibase/1.31/.travis/build-deploy.sh
+++ wikibase/1.31/.travis/build-deploy.sh
@@ -2,14 +2,14 @@
 #Oneline for full directory name see: https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -e
-docker build "$DIR/../base" -t wikibase/wikibase:1.31 -t wikibase/wikibase:1.31-base -t wikibase/wikibase:lts -t wikibase/wikibase:lts-base
-docker build "$DIR/../bundle" -t wikibase/wikibase:1.31-bundle -t wikibase/wikibase:lts-bundle
+docker build "$DIR/../base" -t ${DOCKER_REPO}/wikibase:1.31 -t ${DOCKER_REPO}/wikibase:1.31-base -t ${DOCKER_REPO}/wikibase:lts -t ${DOCKER_REPO}/wikibase:lts-base
+docker build "$DIR/../bundle" -t ${DOCKER_REPO}/wikibase:1.31-bundle -t ${DOCKER_REPO}/wikibase:lts-bundle
 
 if [ "$SHOULD_DOCKER_PUSH" = true ]; then
-    docker push wikibase/wikibase:1.31
-    docker push wikibase/wikibase:lts
-    docker push wikibase/wikibase:1.31-base
-    docker push wikibase/wikibase:lts-base
-    docker push wikibase/wikibase:1.31-bundle
-    docker push wikibase/wikibase:lts-bundle
+    docker push ${DOCKER_REPO}/wikibase:1.31
+    docker push ${DOCKER_REPO}/wikibase:lts
+    docker push ${DOCKER_REPO}/wikibase:1.31-base
+    docker push ${DOCKER_REPO}/wikibase:lts-base
+    docker push ${DOCKER_REPO}/wikibase:1.31-bundle
+    docker push ${DOCKER_REPO}/wikibase:lts-bundle
 fi
