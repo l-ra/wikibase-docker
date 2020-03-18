feab62c938a2aedd5bd221b0a047bf9076ee74c1
lra <lra@r73.info>
2020-03-18 22:40:00 +0100

    merge

--- wikibase/1.33/.travis/build-deploy.sh
+++ wikibase/1.33/.travis/build-deploy.sh
@@ -2,6 +2,7 @@
 #Oneline for full directory name see: https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 set -e
+<<<<<<< HEAD
 docker build "$DIR/../base" -t ${DOCKER_REPO}/wikibase:1.33 -t ${DOCKER_REPO}/wikibase:1.33-base -t ${DOCKER_REPO}/wikibase:latest -t ${DOCKER_REPO}/wikibase:latest-base
 docker build "$DIR/../bundle" -t ${DOCKER_REPO}/wikibase:1.33-bundle -t ${DOCKER_REPO}/wikibase:latest-bundle
 
@@ -12,4 +13,16 @@ if [ "$SHOULD_DOCKER_PUSH" = true ]; then
     docker push ${DOCKER_REPO}/wikibase:latest-base
     docker push ${DOCKER_REPO}/wikibase:1.33-bundle
     docker push ${DOCKER_REPO}/wikibase:latest-bundle
+=======
+docker build "$DIR/../base" -t wikibase/wikibase:1.33 -t wikibase/wikibase:1.33-base -t wikibase/wikibase:legacy -t wikibase/wikibase:legacy-base
+docker build "$DIR/../bundle" -t wikibase/wikibase:1.33-bundle -t wikibase/wikibase:legacy-bundle
+
+if [ "$SHOULD_DOCKER_PUSH" = true ]; then
+    docker push wikibase/wikibase:1.33
+    docker push wikibase/wikibase:legacy
+    docker push wikibase/wikibase:1.33-base
+    docker push wikibase/wikibase:legacy-base
+    docker push wikibase/wikibase:1.33-bundle
+    docker push wikibase/wikibase:legacy-bundle
+>>>>>>> 644495b12565717641d342b452cc79a1eaef08ad
 fi
